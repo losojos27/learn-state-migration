cb40fbf6516c2e19a857e0ab1e3e33ef4286b8ed
Lee Owensby <lee.owensby@gmail.com>
2023-04-03 12:39:06 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,18 +4,40 @@
 ## Terraform configuration
 
 terraform {
+
+  # backend "s3" {
+  #   bucket  = "lo-petgrackle-panera"
+  #   key     = "states"
+  #   region  = "us-east-1"
+  #   profile = "hashicorp-doormat"
+  # }
+
+  backend "gcs" {
+    bucket = "lo-petgrackle-panera"
+    prefix = "terraform/state"
+  }
+
+  # cloud {
+  #   organization = "lo-petgrackle"
+  #   workspaces {
+  #     name = "learn-terraform-cloud-migrate"
+  #   }
+  # }
+
   required_providers {
     random = {
       source  = "hashicorp/random"
       version = "3.3.2"
     }
   }
+
   required_version = ">= 1.1.0"
+
 }
 
 variable "name_length" {
   description = "The number of words in the pet name"
-  default     = "3"
+  default     = "6"
 }
 
 resource "random_pet" "pet_name" {
